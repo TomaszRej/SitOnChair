9734c72389b665881b52a30643664d5edc9ed745
Tomasz Rej <tomaszrej@gmail.com>
2018-10-26 20:30:48 +0200

    js file added

---
+++ js/app.js
@@ -0,0 +1,43 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const productFirst = document.querySelector('.product-first');
+    const productSecond = document.querySelector('.product-second');
+
+    productFirst.addEventListener('mouseover', function () {
+        const strip = this.firstElementChild;
+        strip.style.display = 'none';
+        // console.log('working');
+    });
+    productFirst.addEventListener('mouseout', function () {
+        const strip = this.firstElementChild;
+        strip.style.display = "block";
+    });
+    productSecond.addEventListener('mouseover', function () {
+        const strip = this.firstElementChild;
+        strip.style.display = 'none';
+        // console.log('working');
+    });
+    productSecond.addEventListener('mouseout', function () {
+        const strip = this.firstElementChild;
+        strip.style.display = "block";
+    });
+
+
+    // const li = document.querySelector('header ul li:first-child');
+    // const dropdown = document.querySelector('.around-dropdown');
+    // console.log(dropdown);
+    // dropdown.style.display = 'none';
+    //
+    //
+    // li.addEventListener('mouseenter', function () {
+    //     dropdown.style.display = "block";
+    // });
+    // li.addEventListener('mouseout', function () {
+    //     setTimeout(function () {
+    //         dropdown.style.display = "none";
+    //     },1000)
+    //
+    //
+    // });
+
+
+});
